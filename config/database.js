d41a3a816cc1098dfcd4fe30b4f970f65f94d7d6
sangsloop <renaldienol86@gmail.com>
2026-03-05 19:51:10 +0700

    update config

--- config/database.js
+++ config/database.js
@@ -1,36 +1,10 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+import mysql from "mysql2";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-    // logging: console.log, // Aktifkan logging untuk debug
-    define: {
-      timestamps: true,
-      underscored: true,
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  },
-);
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-// Test koneksi
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("✅ Database connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("❌ Unable to connect to the database:", err);
-  });
-
-module.exports = sequelize;
+module.exports = connection;
