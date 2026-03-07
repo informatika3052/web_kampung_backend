9381d53985149259738f7289df4b49db279a7d4b
renaldi86 <48443731+informatika3052@users.noreply.github.com>
2026-03-07 18:10:21 +0700

    Edit cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
